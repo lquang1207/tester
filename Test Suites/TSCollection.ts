11ec2f78a7aaa2b78b5b8aefc5a9664df0e98ed4
uyenpham <puyenvl2206@gmail.com>
2023-08-09 15:43:44 +0700

    sec-commit

---
+++ Test Suites/TSCollection.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TSCollection</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>afba1c8f-8565-4839-b507-66b9c8796f28</testSuiteGuid>
+</TestSuiteEntity>
